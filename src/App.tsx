6b2ac511b960b6575bbdc70e09f0f03087cfe55b
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2025-03-20 18:24:55 +0200

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
@@ -16,7 +15,6 @@ import Contact from './pages/Contact'
 import Profile from './pages/Profile'
 import { useAuth } from './hooks/useAuth'
 import Checkout from './pages/Checkout'
-import { Toaster } from 'sonner'
 
 export default function App() {
   const { initialize } = useAuth()
@@ -31,7 +29,7 @@ export default function App() {
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            <Route path="/Carvy" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/categories" element={<Categories />} />
             <Route path="/categories/:categoryId" element={<CategoryShops />} />
             <Route path="/shops" element={<Shops />} />
@@ -46,7 +44,6 @@ export default function App() {
           </Routes>
         </main>
         <Footer />
-        <Toaster position="top-right" />
       </div>
     </Router>
   )
